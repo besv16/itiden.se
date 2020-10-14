1acafe2c99ce51bccb59ae41e9a2bc06c9e3ddc8
Beatrice Svensson <beatrice@itiden.se>
2020-10-14 11:29:05 +0200

    Add dynamic color to Contact component

--- components/Layout/Contact.tsx
+++ components/Layout/Contact.tsx
@@ -4,29 +4,52 @@ import tw from 'twin.macro';
 import { Content, Hero } from '../Layout';
 import { ContactsGroup } from '../../components/ContactIcons/ContactsGroup';
 
+interface ContactProps {
+  blue?: boolean;
+  green?: boolean;
+}
+
 const ContactWrapper = styled.div`
-  ${tw`mb-32 hidden sm:block`}
+  ${tw`hidden sm:block`}
 `;
 
 const GreenBanner = styled(Hero)`
   ${tw`py-10 bg-green text-center`};
 `;
 
+const BlueBanner = styled(Hero)`
+  ${tw`py-10 bg-teal-400 text-center`};
+`;
+
 const GrayBanner = styled(Hero)`
   ${tw`py-10 bg-gray-600 text-center`};
 `;
 
-export const Contact = () => (
+export const Contact: React.FC<ContactProps> = props => {
+return (
   <ContactWrapper>
-    <GreenBanner>
-      <Content>
-        <p>Kontakta oss och berätta mer om dig och dina behov.</p>
-      </Content>
-    </GreenBanner>
+      { props.green &&
+        <>
+          <GreenBanner>
+            <Content>
+              <p>Kontakta oss och berätta mer om dig och dina behov.</p>
+            </Content>
+          </GreenBanner>
+        </>
+      }
+      { props.blue &&
+        <>
+          <BlueBanner>
+            <Content>
+              <p>Kontakta oss och berätta mer om dig och dina behov.</p>
+            </Content>
+          </BlueBanner>
+        </>
+      }
     <GrayBanner>
       <Content>
         <ContactsGroup mail phone />
       </Content>
     </GrayBanner>
   </ContactWrapper>
-);
+)};
