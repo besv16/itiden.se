b898e64e41a24c83516938064fd0960f32e9c737
Beatrice Svensson <beatrice@itiden.se>
2020-10-14 12:56:14 +0200

    Add styling rules to H3

--- components/Layout/H3.tsx
+++ components/Layout/H3.tsx
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const H3 = styled.h3`
-    ${tw`text-h3`}
+    ${tw`text-2xl font-normal sm:(text-h3 font-semibold)`}
 `;
