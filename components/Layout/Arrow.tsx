dee4181cdf0dde86cf5c0c14f71f1b7e272bd68b
Beatrice Svensson <beatrice@itiden.se>
2020-10-15 10:25:59 +0200

    Add padding bottom to Arrow

--- components/Layout/Arrow.tsx
+++ components/Layout/Arrow.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const ArrowContainer = styled.div`
-  ${tw`sm:hidden flex justify-center mb-6`}
+  ${tw`sm:hidden flex justify-center mb-6 pb-16`}
 `;
 
 export const Arrow = () => (
