866bf3fde29641631213ae03b7de855058dfe64f
Andreas Bergqvist <andreas@itiden.se>
2020-10-15 17:04:24 +0200

    Update nextjs and other modules

--- next.config.js
+++ next.config.js
@@ -1,85 +1,81 @@
 const withCSS = require('@zeit/next-css');
-const withOptimizedImages = require('next-optimized-images');
-const withTM = require('next-transpile-modules');
+const withTM = require('next-transpile-modules')(['tailwindcss']);
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const cases = require('./data/data/case.json');
+//const cases = require('./data/data/case.json');
 
 module.exports = withBundleAnalyzer(
   withCSS(
-    withOptimizedImages(
-      withTM({
-        transpileModules: ['tailwindcss'],
-        trailingSlash: true,
-        optimizeImages: false,
-        experimental: {
-          modern: true,
-        },
-        exportPathMap: async function() {
-          const paths = {
-            '/': { page: '/' },
-            '/case': { page: '/case' },
-            '/om': { page: '/om' },
-            '/kontakt': { page: '/kontakt' },
-            // '/jobb-webbutvecklare': { page: '/jobb-webbutvecklare' },
-            '/jobb-tackar': { page: '/jobb-tackar' },
+    withTM({
+      trailingSlash: true,
+      experimental: {
+        modern: true,
+      },
+      /*
+      exportPathMap: async function() {
+        const paths = {
+          '/': { page: '/' },
+          '/case': { page: '/case' },
+          '/om': { page: '/om' },
+          '/kontakt': { page: '/kontakt' },
+          '/jobb-tackar': { page: '/jobb-tackar' },
+        };
+        cases.forEach(c => {
+          paths[`/case/${c.slug}`] = {
+            page: '/case/[slug]',
+            query: { slug: c.slug },
           };
-          cases.forEach(c => {
-            paths[`/case/${c.slug}`] = {
-              page: '/case/[slug]',
-              query: { slug: c.slug },
-            };
-          });
-          return paths;
-        },
-        webpack: config => {
-          config.plugins = config.plugins || [];
+        });
+        return paths;
+      },
+      */
+      webpack: config => {
+        config.plugins = config.plugins || [];
 
-          config.plugins = [
-            ...config.plugins,
+        config.plugins = [
+          ...config.plugins,
 
-            // Read the .env file
-            new Dotenv({
-              path: path.join(__dirname, '.env'),
-              systemvars: true,
-            }),
-          ];
+          // Read the .env file
+          new Dotenv({
+            path: path.join(__dirname, '.env'),
+            systemvars: true,
+          }),
+        ];
 
-          config.resolve.alias = {
-            ...config.resolve.alias,
-            'react-spring$': require.resolve('react-spring/web.cjs'),
-            'react-spring/renderprops$': require.resolve(
-              'react-spring/renderprops.cjs'
-            ),
-            // 'react-use-gesture': require.resolve('react-use-gesture/web.cjs')
-          };
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          'react-spring$': require.resolve('react-spring/web.cjs'),
+          'react-spring/renderprops$': require.resolve(
+            'react-spring/renderprops.cjs'
+          ),
+          // 'react-use-gesture': require.resolve('react-use-gesture/web.cjs')
+        };
 
-          const originalEntry = config.entry;
-          config.entry = async () => {
-            const entries = await originalEntry();
+        const originalEntry = config.entry;
+        config.entry = async () => {
+          const entries = await originalEntry();
 
-            if (
-              entries['main.js'] &&
-              !entries['main.js'].includes('./polyfills.js')
-            ) {
-              entries['main.js'].unshift('./polyfills.js');
-            }
+          if (
+            entries['main.js'] &&
+            !entries['main.js'].includes('./polyfills.js')
+          ) {
+            entries['main.js'].unshift('./polyfills.js');
+          }
 
-            return entries;
-          };
+          return entries;
+        };
 
-          config.node = {
-            ...config.node,
-            fs: 'empty',
-          };
+        config.node = {
+          ...config.node,
+          fs: 'empty',
+        };
 
-          return config;
-        },
-      })
-    )
+        return config;
+      },
+    })
   )
 );
