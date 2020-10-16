c4065a24133887017e56d80f9bda86fd325133da
Beatrice Svensson <beatrice@itiden.se>
2020-10-16 10:31:40 +0200

    Update error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,10 @@
 import { GetStaticProps, NextComponentType } from 'next';
-import Head from 'next/head';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { CaseGrid } from '../components/Case';
-import { Content, Header, Page } from '../components/Layout';
+import { IndexHeader } from '../components/IndexHeader';
+import { Header, Page, Content, H2, H3 } from '../components/Layout';
 import { getCasesFromJson } from '../data/case';
 import { Case } from '../models';
 
@@ -12,14 +12,26 @@ interface ErrorPageProps {
   cases: Case[];
 }
 
-const IntroText = styled(Content)`
-  ${tw`text-lg md:text-2xl text-gray-500 tracking-wide`}
+const ContentWrapper = styled.div<{
+  paddingTop: string;
+  paddingBottom: string;
+}>`
+  padding-top: ${props => props.paddingTop};
+  padding-bottom: ${props => props.paddingBottom};
+`;
 
-  & b {
-    ${tw`font-bold text-gray-500`}
-  }
-  & a {
-    ${tw`font-bold`}
+const CaseGridWrapper = styled.div`
+  ${tw`mt-16 md:mt-6`}
+`;
+
+const Paragraph = styled.p<{ paddingBottom?: string }>`
+  ${tw`text-lg text-gray-300 mt-1 sm:text-xl w-full md:w-3/4 lg:w-3/5`}
+  ${props =>
+    props.paddingBottom != null && `padding-bottom: ${props.paddingBottom}`};
+
+  @media (max-width: 767px) {
+    margin-bottom: 0;
+    padding-bottom: 0;
   }
 `;
 
@@ -28,13 +40,34 @@ const ErrorPage: NextComponentType<{}, {}, ErrorPageProps> = ({ cases }) => {
     <Page>
       <IndexHeader />
       <Header role="banner">
-        <IntroText>
-          <h3>Oooops!</h3>
-          <h1>Sidan hittades inte</h1>
-          <p>Du kanske letade efter något av våra case?</p>
-        </IntroText>
+        <ContentWrapper paddingTop={'16rem'} paddingBottom={'0.5rem'}>
+          <Content>
+            <H2>404</H2>
+            <Paragraph paddingBottom={'1.5rem'}>
+              Sedärja, den sidan hittades tyvärr inte. Kanske letar du efter något av våra case, de finner du isf nedan.
+            </Paragraph>
+          </Content>
+        </ContentWrapper>
       </Header>
-      <CaseGrid cases={cases} />
+      <Content>
+        <CaseGrid cases={cases.filter(c => !c.labs)} />
+      </Content>
+      <ContentWrapper paddingTop={'6rem'} paddingBottom={'0.5rem'} id="lab">
+        <Content>
+          <H3>Itiden Lab</H3>
+          <Paragraph>
+            Det händer ibland att vi bygger egna produkter eller tjänster. Det
+            kan vara för att utmana oss, lära oss nya tekniker eller helt enkelt
+            för att vi tycker produkten behövs. Denna utveckling samlar vi under
+            vad vi kallar Itiden Lab.
+          </Paragraph>
+        </Content>
+      </ContentWrapper>
+      <Content>
+        <CaseGridWrapper>
+          <CaseGrid cases={cases.filter(c => c.labs)} />
+        </CaseGridWrapper>
+      </Content>
     </Page>
   );
 };
@@ -52,19 +85,3 @@ export const getStaticProps: GetStaticProps<Omit<
 };
 
 export default ErrorPage;
-
-const IndexHeader: React.FC<{}> = () => (
-  <Head>
-    <title>Itiden - Webbutveckling | Apputveckling | Göteborg</title>
-    <meta
-      name="Description"
-      content="Itiden är en digital produktionsbyrå med ett team av webbutvecklare och apputvecklare i Göteborg. Vi jobbar tillsammans med produktbolag, startups och byråer och hjälper med utveckling av webbsidor, webbapplikation och mobilappar."
-    />
-    <meta
-      name="title"
-      property="og:title"
-      content="Itiden - Webbutveckling | Apputveckling | Göteborg"
-    />
-    <meta name="image" property="og:image" content="/static/itiden-share.png" />
-  </Head>
-);
