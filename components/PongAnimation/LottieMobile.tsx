7be18d5e6282f25e804ff7646cceac83037a6be5
Beatrice Svensson <beatrice@itiden.se>
2020-10-16 15:54:34 +0200

    Add mobile pong animation

--- components/PongAnimation/LottieMobile.tsx
+++ components/PongAnimation/LottieMobile.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Lottie } from '@crello/react-lottie';
-import animationData from '../../public/static/pongDataDesktop.json';
-// import animationData from '../../public/static/pongDataMobile.json';
+import animationData from '../../public/static/pongDataMobile.json';
 
 export const LottieMobile = () => {
   return <Lottie config={{ animationData, autoplay: true, loop: true }} />;
