3f2703287dbc0d1ed2dd6d5f504d7bd2074d5bd1
Beatrice Svensson <beatrice@itiden.se>
2020-10-21 10:38:20 +0200

    Iconsgroup mobile adjustments

--- components/AboutIcons/IconsGroup.tsx
+++ components/AboutIcons/IconsGroup.tsx
@@ -4,6 +4,10 @@ import tw from 'twin.macro';
 import { App, System, Webb } from '../AboutIcons';
 import { H3, Content } from '../Layout';
 
+const IconWrapper = styled.div`
+  ${tw`ml-6 sm:ml-0`};
+`;
+
 const AppIcon = styled(App)`
   ${tw``};
 `;
@@ -17,32 +21,41 @@ const WebbIcon = styled(Webb)`
 `;
 
 const TechBoxWrapper = styled.div`
-  ${tw`flex flex-col md:(flex-row justify-between)`};
+  ${tw`flex flex-col md:(flex-row justify-between)`}
 `;
 
 const TechBox = styled.div`
-  ${tw`items-center pb-24 flex flex-col lg:w-1/3`}
+  ${tw`items-start sm:items-center pb-24 flex flex-col lg:w-1/3`}
 `;
 
 const TechBoxMiddle = styled.div`
-  ${tw`items-center pb-24 flex flex-col md:(w-1/3 mx-16 pb-32) lg:mx-20`}
+  ${tw`items-start sm:items-center pb-24 flex flex-col md:(w-1/3 mx-16 pb-32) lg:mx-20`}
 `;
 
 const TechHeading = styled(H3)`
-  ${tw`my-6 md:my-5`}
+  ${tw`mt-6 mb-3 md:(my-5)`}
 `;
 
 const TechInfo = styled.div`
-  ${tw`text-center text-gray-300`}
-  max-width: 327px;
+  ${tw`text-left sm:text-center text-gray-300`}
+
+  @media (min-width: 480px) and (max-width: 639px) {
+    ${tw`w-4/5`}
+  }
+  
+  @media (min-width: 640px) {
+    max-width: 327px;
+  }
 `;
 
 export const AboutIconsGroup = () => (
   <Content>
     <TechBoxWrapper>
       <TechBox>
-        <AppIcon />
-        <TechHeading>App</TechHeading>
+        <IconWrapper>
+          <AppIcon />
+        </IconWrapper>
+        <TechHeading>Apputveckling</TechHeading>
         <TechInfo>
           <p>
             Låt en app ta era tjänster till nästa nivå. Vi på Itiden är uppdaterade
@@ -51,8 +64,10 @@ export const AboutIconsGroup = () => (
         </TechInfo>
       </TechBox>
       <TechBoxMiddle>
-        <WebbIcon />
-        <TechHeading>Webb</TechHeading>
+        <IconWrapper>
+          <WebbIcon />
+        </IconWrapper>
+        <TechHeading>Webbutveckling</TechHeading>
         <TechInfo>
           <p>
             Rätt webblösning för varje behov. Vi hjälper till från grunden med
@@ -61,8 +76,10 @@ export const AboutIconsGroup = () => (
         </TechInfo>
       </TechBoxMiddle>
       <TechBox>
-        <SystemIcon />
-        <TechHeading>System</TechHeading>
+        <IconWrapper>
+          <SystemIcon />
+        </IconWrapper>
+        <TechHeading>Systemintegration</TechHeading>
         <TechInfo>
           <p>
             Backend, frontend, server, serverless och cloud. Databaser och api etc.
