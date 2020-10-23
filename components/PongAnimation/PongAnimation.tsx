d5e7a142f88b6da8bc47ac6911bf73165f0eab90
Beatrice Svensson <beatrice@itiden.se>
2020-10-23 10:39:41 +0200

    Set full width in mobile pong wrapper

--- components/PongAnimation/PongAnimation.tsx
+++ components/PongAnimation/PongAnimation.tsx
@@ -9,7 +9,7 @@ const OuterWrapper = styled(Hero)`
   ${tw`bg-gray-700 py-4`}
 `;
 const WrapperMobile = styled.div`
-  ${tw`sm:hidden`}
+  ${tw`w-full sm:hidden`}
 `;
 const WrapperDesktop = styled.div`
   ${tw`hidden px-6 sm:(block px-5) lg:px-0`}
