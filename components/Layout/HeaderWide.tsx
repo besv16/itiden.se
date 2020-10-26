ee782b6393f3d0071246737231405152621e07f1
Beatrice Svensson <beatrice@itiden.se>
2020-10-26 11:02:22 +0100

    Set min height on HeaderWide

--- components/Layout/HeaderWide.tsx
+++ components/Layout/HeaderWide.tsx
@@ -3,13 +3,25 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const Wrapper = styled.div<{ height: string }>`
+  ${tw`flex flex-col justify-end -mt-10`}
   height: ${p => p.height};
-  ${tw`flex flex-col justify-end sm:pt-48 -mt-10`}
 
-  @media (max-width: 767px) {
-    ${tw`justify-end`}
+  @media screen and (min-width: 640px) {
+    ${tw`pt-48`}
+    min-height: 560px;
     margin-top: -144px;
   }
+
+  @media screen and (max-width: 639px) {
+    min-height: 480px;
+    margin-top: -144px;
+  }
+
+  @media screen and (min-width: 768px) {
+    ${tw`-mt-10`}
+    min-height: 820px; 
+  }
+
   & p {
     ${tw`w-full md:w-3/4 lg:w-3/5`}
   }
