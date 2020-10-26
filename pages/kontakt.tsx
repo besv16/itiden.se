d9c3e381c234404b1bd7ab5c634738c2b7697bad
Beatrice Svensson <beatrice@itiden.se>
2020-10-26 13:31:41 +0100

    Custom screen height fixes on kontakt

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -30,8 +30,8 @@ const ContentWrapper = styled.div`
 const QuarteredContent = styled.div`
   ${tw`flex flex-col pt-56 md:(w-3/4 pt-0)`}
 
-  @media screen and (max-height: 480px) {
-    ${tw`pt-20`}
+  @media screen and (max-height: 580px) {
+    ${tw`pt-40 pb-32`}
   }
 `;
 const GrayBanner = styled(Hero)`
